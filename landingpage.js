5714ec3d64467ca1dfd0f0f68274a4eb23b7527e
premganwani619 <83330321+premganwani619@users.noreply.github.com>
2023-11-06 18:11:23 +0530

    Update landingpage.js

--- landingpage.js
+++ landingpage.js
@@ -1,78 +1,86 @@
+var itemsPerPage = 4; 
 var currentPage = 1;
 
-        function fetchUsers(page) {
-            fetch(`https://reqres.in/api/users?page=${page}&per_page=4`)
-                .then(response => response.json())
-                .then(data => {
-                    populateUserGrid(data.data);
-                    updatePagination(data.total_pages);
-                })
-                .catch(error => console.log(error));
+function changeItemsPerPage(value) {
+    itemsPerPage = parseInt(value);
+    currentPage = 1;
+    fetchUsers(currentPage);
+}
+
+function fetchUsers(page) {
+    fetch(`https://reqres.in/api/users?page=${page}&per_page=${itemsPerPage}`)
+        .then(response => response.json())
+        .then(data => {
+            populateUserGrid(data.data);
+            updatePagination(data.total_pages);
+        })
+        .catch(error => console.log(error));
+}
+
+function populateUserGrid(users) {
+    var userGrid = document.getElementById("user-grid");
+    userGrid.innerHTML = "";
+
+    users.forEach(user => {
+        var userCard = document.createElement("div");
+        userCard.classList.add("card");
+        userCard.innerHTML = `
+        <img src="${user.avatar}" class="card-img-top" alt="User Avatar">
+        <div class="card-body">
+            <h5 class="card-title">${user.first_name} ${user.last_name}</h5>
+            <p class="card-text">${user.email}</p>
+        </div>
+        `;
+        userGrid.appendChild(userCard);
+    });
+}
+
+function updatePagination(totalPages) {
+    var pagination = document.getElementById("pagination");
+    pagination.innerHTML = "";
+
+    var previousButton = document.createElement("li");
+    previousButton.classList.add("page-item");
+    previousButton.innerHTML = `<a class="page-link" href="#" onclick="changePage(${currentPage - 1})">Previous</a>`;
+    pagination.appendChild(previousButton);
+
+    for (var i = 1; i <= totalPages; i++) {
+        var pageItem = document.createElement("li");
+        pageItem.classList.add("page-item");
+        if (i === currentPage) {
+            pageItem.classList.add("active");
         }
+        pageItem.innerHTML = `<a class="page-link" href="#" onclick="changePage(${i})">${i}</a>`;
+        pagination.appendChild(pageItem);
+    }
 
-        function populateUserGrid(users) {
-            var userGrid = document.getElementById("user-grid");
-            userGrid.innerHTML = "";
+    var nextButton = document.createElement("li");
+    nextButton.classList.add("page-item");
+    nextButton.innerHTML = `<a class="page-link" href="#" onclick="changePage(${currentPage + 1})">Next</a>`;
+    pagination.appendChild(nextButton);
 
-            users.forEach(user => {
-                var userCard = document.createElement("div");
-                userCard.classList.add("card");
-                userCard.innerHTML = `
-      <img src="${user.avatar}" class="card-img-top" alt="User Avatar">
-      <div class="card-body">
-        <h5 class="card-title">${user.first_name} ${user.last_name}</h5>
-        <p class="card-text">${user.email}</p>
-      </div>
-    `;
-                userGrid.appendChild(userCard);
-            });
-        }
+    var previousPageButton = document.querySelector(".page-item:first-child");
+    var nextPageButton = document.querySelector(".page-item:last-child");
 
-        function updatePagination(totalPages) {
-            var pagination = document.getElementById("pagination");
-            pagination.innerHTML = "";
+    if (currentPage === 1) {
+        previousPageButton.classList.add("disabled");
+    } else {
+        previousPageButton.classList.remove("disabled");
+    }
 
-            var previousButton = document.createElement("li");
-            previousButton.classList.add("page-item");
-            previousButton.innerHTML = `<a class="page-link" href="#" onclick="changePage(${currentPage - 1})">Previous</a>`;
-            pagination.appendChild(previousButton);
+    if (currentPage === totalPages) {
+        nextPageButton.classList.add("disabled");
+    } else {
+        nextPageButton.classList.remove("disabled");
+    }
+}
 
-            for (var i = 1; i <= totalPages; i++) {
-                var pageItem = document.createElement("li");
-                pageItem.classList.add("page-item");
-                if (i === currentPage) {
-                    pageItem.classList.add("active");
-                }
-                pageItem.innerHTML = `<a class="page-link" href="#" onclick="changePage(${i})">${i}</a>`;
-                pagination.appendChild(pageItem);
-            }
+function changePage(page) {
+    if (page < 1) {
+        return;
+    }
+    currentPage = page;
+    fetchUsers(currentPage);
+}
 
-            var nextButton = document.createElement("li");
-            nextButton.classList.add("page-item");
-            nextButton.innerHTML = `<a class="page-link" href="#" onclick="changePage(${currentPage + 1})">Next</a>`;
-            pagination.appendChild(nextButton);
-
-            var previousPageButton = document.querySelector(".page-item:first-child");
-            var nextPageButton = document.querySelector(".page-item:last-child");
-
-            if (currentPage === 1) {
-                previousPageButton.classList.add("disabled");
-            } else {
-                previousPageButton.classList.remove("disabled");
-            }
-
-            if (currentPage === totalPages) {
-                nextPageButton.classList.add("disabled");
-            } else {
-                nextPageButton.classList.remove("disabled");
-            }
-        }
-
-        function changePage(page) {
-            if (page < 1) {
-                return;
-            }
-            currentPage = page;
-            fetchUsers(currentPage);
-        }
-        fetchUsers(currentPage);
+fetchUsers(currentPage);
